c21005d8954fd1cea2daa947c444c4ba36f30b45
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-16 12:52:47 +0700

    feat: add nearby event form skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 
 import { Banner } from "../components/Banner"
+import { NearbyEventForm } from "../components/NearbyEvent"
 
 export const HomePage: React.FC = () => {
   return (
     <div>
       <Banner />
+      <NearbyEventForm />
       home
     </div>
   )
