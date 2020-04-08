1a01f1c40f8955403671bdc61d88970ccb48eeee
Ryan-Willpower <surasak.c.work@gmail.com>
2020-04-08 23:21:14 +0700

    chore: refactor

--- components/info/index.tsx
+++ components/info/index.tsx
@@ -1,89 +1,4 @@
-import styled from "styled-components"
-
-const InfoContainer = styled.div`
-  margin-top: 30px;
-  background: url("/images/kid.png") center center;
-  background-size: cover;
-  font-family: "SukhumvitSet", Arial, Helvetica, sans-serif;
-  padding: 20px 0 50px;
-
-  h1 {
-    font-size: 16px;
-    font-weight: 600;
-    width: 100%;
-    text-align: center;
-  }
-
-  @media screen and (min-width: 769px) {
-    h1 {
-      font-size: 24px;
-    }
-  }
-
-  @media screen and (min-width: 1025px) {
-    padding: 30px 0 100px;
-
-    h1 {
-      font-size: 32px;
-    }
-  }
-`
-
-const WrapContainer = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
-  padding: 0 20px;
-  margin-top: 20px;
-
-  @media screen and (min-width: 769px) {
-    margin-top: 50px;
-    padding: 0 60px;
-    grid-gap: 120px;
-  }
-`
-
-const Card = styled.div`
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  text-align: center;
-
-  > img {
-    width: 50px;
-    height: 50px;
-  }
-
-  > h1 {
-    margin-bottom: 0px;
-  }
-
-  > p {
-    font-size: 14px;
-  }
-
-  @media screen and (min-width: 769px) {
-    > img {
-      width: 110px;
-      height: 110px;
-    }
-
-    > p {
-      font-size: 16px;
-    }
-  }
-
-  @media screen and (min-width: 1025px) {
-    > img {
-      width: 220px;
-      height: 220px;
-    }
-
-    > p {
-      font-size: 18px;
-    }
-  }
-`
+import { InfoContainer, WrapContainer, Card } from "./styles/info"
 
 export const Info: React.FC = () => (
   <InfoContainer>
@@ -98,6 +13,7 @@ export const Info: React.FC = () => (
           เพื่อประเมินพฤติกรรมของลูก
         </p>
       </Card>
+
       <Card>
         <img src="/images/soccer.png" />
         <h1>กิจกรรมที่แนะนำ</h1>
@@ -107,6 +23,7 @@ export const Info: React.FC = () => (
           ที่ถูกคัดสรรมาเพื่อลูกคุณโดยเฉพาะ
         </p>
       </Card>
+
       <Card>
         <img src="/images/up.png" />
         <h1>ติดตามผล</h1>
---
+++ components/info/styles/info.ts
@@ -0,0 +1,86 @@
+import styled from "styled-components"
+
+export const InfoContainer = styled.div`
+  margin-top: 30px;
+  background: url("/images/kid.png") center center;
+  background-size: cover;
+  font-family: "SukhumvitSet", Arial, Helvetica, sans-serif;
+  padding: 20px 0 50px;
+
+  h1 {
+    font-size: 16px;
+    font-weight: 600;
+    width: 100%;
+    text-align: center;
+  }
+
+  @media screen and (min-width: 769px) {
+    h1 {
+      font-size: 24px;
+    }
+  }
+
+  @media screen and (min-width: 1025px) {
+    padding: 30px 0 100px;
+
+    h1 {
+      font-size: 32px;
+    }
+  }
+`
+
+export const WrapContainer = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 0 20px;
+  margin-top: 20px;
+
+  @media screen and (min-width: 769px) {
+    margin-top: 50px;
+    padding: 0 60px;
+    grid-gap: 120px;
+  }
+`
+
+export const Card = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  text-align: center;
+
+  > img {
+    width: 50px;
+    height: 50px;
+  }
+
+  > h1 {
+    margin-bottom: 0px;
+  }
+
+  > p {
+    font-size: 14px;
+  }
+
+  @media screen and (min-width: 769px) {
+    > img {
+      width: 110px;
+      height: 110px;
+    }
+
+    > p {
+      font-size: 16px;
+    }
+  }
+
+  @media screen and (min-width: 1025px) {
+    > img {
+      width: 220px;
+      height: 220px;
+    }
+
+    > p {
+      font-size: 18px;
+    }
+  }
+`
