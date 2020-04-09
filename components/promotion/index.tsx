d846b46a25afaa6425ae6ef062852718836dd9b9
Ryan-Willpower <surasak.c.work@gmail.com>
2020-04-10 01:10:10 +0700

    chore: add space to easiness to read

--- components/promotion/index.tsx
+++ components/promotion/index.tsx
@@ -17,20 +17,24 @@ export const Promotion: React.FC = () => (
   <Container>
     <Upper>
       <SectionTitle title="โปรโมชั่นประจำเดือน" />
+
       <Menu>
         <MenuItem isSelected={true}>ข้อเสนอพิเศษ</MenuItem>
         <MenuItem>วันหยุดสุดพิเศษ</MenuItem>
         <span>ดูโปรโมชั่นทั้งหมด</span>
       </Menu>
     </Upper>
+
     <MainContent>
       <LeftSection>
         <img src="/images/cover-slide.png" alt="cover-slide" />
+
         <Navigation>
           <div className="button-group">
             <img className="left" src="/svg/left.svg" alt="left" />
             <img className="right" src="/svg/right.svg" alt="" />
           </div>
+
           <div className="pages-group">
             <Pagination className="selected" />
             <Pagination />
@@ -39,8 +43,10 @@ export const Promotion: React.FC = () => (
           </div>
         </Navigation>
       </LeftSection>
+
       <RightSection>
         <SectionTitle title="Lorem Ipsum" />
+
         <p>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
@@ -48,6 +54,7 @@ export const Promotion: React.FC = () => (
           rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
           ipsum dolor sit amet.{" "}
         </p>
+
         <Button>อ่านต่อ</Button>
       </RightSection>
     </MainContent>
