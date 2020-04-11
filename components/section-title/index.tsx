66b3b2934b6289ab336cc93055c85f79e9c84751
Ryan-Willpower <surasak.c.work@gmail.com>
2020-04-11 18:39:55 +0700

    chore: make section title style reusable

--- components/section-title/index.tsx
+++ components/section-title/index.tsx
@@ -2,9 +2,13 @@ import { Title } from "./styles/section-title"
 import { RedLine } from "../../styles/red-headline"
 
 import { SectionTitleProps } from "./types"
+import { CSSProps } from "../layout/types"
 
-export const SectionTitle: React.FC<SectionTitleProps> = ({ title }) => (
-  <Title>
+export const SectionTitle: React.FC<SectionTitleProps & CSSProps> = ({
+  title,
+  style,
+}) => (
+  <Title css={style}>
     <h1>{title}</h1>
     <RedLine />
   </Title>
