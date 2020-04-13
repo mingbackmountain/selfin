84baf607747aa50bb56b327d5e04c31d2a40cbd2
Ryan-Willpower <surasak.c.work@gmail.com>
2020-04-14 01:23:43 +0700

    feat: add event description components

---
+++ components/event-description/index.tsx
@@ -0,0 +1,16 @@
+import { SectionTitle } from "../section-title"
+
+import { DescriptionContainer } from "./styles/event-description"
+
+export const EventDescription = () => (
+  <DescriptionContainer>
+    <SectionTitle title="ความน่าสนใจของกิจกรรม" />
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+    </p>
+  </DescriptionContainer>
+)
---
+++ components/event-description/styles/event-description.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+
+export const DescriptionContainer = styled.div`
+  background-color: #ffc626;
+  padding: 20px 120px 50px;
+
+  p {
+    width: 50%;
+    margin-top: 40px;
+  }
+`
