f34daed269dfaeebd9b98e0b6a361162d5291820
Ryan-Willpower <surasak.c.work@gmail.com>
2020-04-15 01:55:08 +0700

    chore: change line height on mobile

--- styles/underline.ts
+++ styles/underline.ts
@@ -6,7 +6,11 @@ interface LineProps {
 
 export const UnderLine = styled.div<LineProps>`
   width: 70%;
-  height: 5px;
+  height: 2px;
   background-color: ${props => (props.color === "red" ? `#f23318` : `#f2d600`)};
   border-radius: 40px;
+
+  @media screen and (min-width: 769px) {
+    height: 5px;
+  }
 `
